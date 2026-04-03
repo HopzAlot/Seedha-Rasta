0865e73aa5713b94173296eb48df6964788110b0
HopzAlot <rehansaqib2006@gmail.com>
2026-04-04 02:17:58 +0500

    updated settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,13 +32,15 @@ CORS_ORIGIN_ALLOW_ALL = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "http://seedha-frontend:5173" 
+    "http://seedha-frontend:5173",
+    "http://seedha-rasta-frontend.onrender.com"
 ]
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://seedha-frontend:5173"
+    "http://seedha-frontend:5173",
+    "http://seedha-rasta-frontend.onrender.com"
 ]
 CSRF_COOKIE_SAMESITE = 'None'
 
