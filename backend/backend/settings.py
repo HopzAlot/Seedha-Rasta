966050f763afe417a6be3696e2b303d1ae9d7ccc
HopzAlot <rehansaqib2006@gmail.com>
2026-04-04 03:48:38 +0500

    A very stupid mistake http instead of https in cors allowed orgins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,8 +33,8 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://seedha-frontend:5173",
-    "http://seedha-rasta-frontend.onrender.com",
-    "http://seedha-rasta.onrender.com",
+    "https://seedha-rasta-frontend.onrender.com",
+    "https://seedha-rasta.onrender.com",
     
 ]
 CORS_ALLOW_ALL_ORIGINS = False
@@ -42,8 +42,8 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://seedha-frontend:5173",
-    "http://seedha-rasta-frontend.onrender.com",
-    "http://seedha-rasta.onrender.com",
+    "https://seedha-rasta-frontend.onrender.com",
+    "https://seedha-rasta.onrender.com",
 ]
 CSRF_COOKIE_SAMESITE = 'None'
 
