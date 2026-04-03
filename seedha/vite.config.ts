303c685bbf13e5532ec99a3506a55cf9da8d35e2
HopzAlot <rehansaqib2006@gmail.com>
2026-04-04 02:16:35 +0500

    updated frontend for deployemnt

--- seedha/vite.config.ts
+++ seedha/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://seedha-backend:8000',
+        target: 'https://seedha-raasta-backend.onrender.com',
         changeOrigin: true,
       },
     },
